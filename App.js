b98b186806f5e8cd0e4ddb55b772c182fed0d1c3
alanmgg <alanfmorag@gmail.com>
2022-07-06 22:37:20 -0500

    Filter function

--- App.js
+++ App.js
@@ -1,24 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet, TextInput, StatusBar } from "react-native";
 
-function App () {
+import CoinItem from "./components/CoinItem";
+
+function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     loadData();
   }, []);
 
-  async function loadData () {
-    const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+  useEffect(() => {
+    console.log(coins.filter((coin) => coin.name.toLowerCase().includes(search)));
+  }, [search]);
+
+  async function loadData() {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
     const data = await res.json();
     setCoins(data);
   }
 
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#141414" />
+      <View style={styles.header}>
+        <Text style={styles.title}>Crypto Market</Text>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search a coin..."
+          placeholderTextColor="#858585"
+          onChangeText={(text) => setSearch(text)}
+        />
+      </View>
+      <FlatList
+        style={styles.list}
+        data={coins.filter((coin) => coin.name.toLowerCase().includes(search) || coin.symbol.toLowerCase().includes(search))}
+        renderItem={({ item }) => {
+          return <CoinItem coin={item} />;
+        }}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
-  )
+  );
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#141414",
+    alignItems: "center",
+    flex: 1,
+  },
+  title: {
+    color: "#FFFFFF",
+    marginTop: 10,
+    fontSize: 20,
+  },
+  list: {
+    width: "90%",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+    marginBottom: 10,
+  },
+  searchInput: {
+    color: "#FFFFFF",
+    borderBottomColor: "#4657CE",
+    borderBottomWidth: 1,
+    width: "40%",
+    textAlign: "center",
+  },
+});
+
+export default App;
---
+++ components/CoinItem.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+function CoinItem({ coin }) {
+  const [coins, setCoins] = useState();
+
+  useEffect(() => {
+    setCoins(coin);
+  }, [coin]);
+
+  if (coins !== undefined) {
+    return (
+      <View style={styles.containerItem}>
+        <View style={styles.coinName}>
+          <Image style={styles.image} source={{ uri: coins.image }} />
+          <View style={styles.containerNames}>
+            <Text style={styles.text}>{coins.name}</Text>
+            <Text style={styles.textSymbol}>{coins.symbol}</Text>
+          </View>
+        </View>
+
+        <View>
+          <Text style={styles.textPrice}>$ {coins.current_price}</Text>
+          <Text
+            style={[
+              styles.pricePercentage,
+              coins.price_change_percentage_24h > 0
+                ? styles.priceUp
+                : styles.priceDown,
+            ]}
+          >
+            {coins.price_change_percentage_24h}
+          </Text>
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.containerItem}>
+        <Text>Hola mundo!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerItem: {
+    backgroundColor: "#121212",
+    paddingTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  containerNames: {
+    marginLeft: 10,
+  },
+  coinName: {
+    flexDirection: "row",
+  },
+  image: {
+    width: 30,
+    height: 30,
+  },
+  text: {
+    color: "#FFFFFF",
+  },
+  textSymbol: {
+    color: "#434343",
+    textTransform: "uppercase",
+  },
+  textPrice: {
+    color: "#FFFFFF",
+    textAlign: "right",
+  },
+  pricePercentage: {
+    textAlign: "right",
+  },
+  priceUp: {
+    color: "#00B5B9",
+  },
+  priceDown: {
+    color: "#FC4422",
+  },
+});
+
+export default CoinItem;
